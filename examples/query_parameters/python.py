b77c1e7500cbc4c3a73beaf69c80a7ce11c61dea
Alair Dias Junior <42477215+alairjunior@users.noreply.github.com>
2021-01-22 18:19:33 -0500

    Update python.py

--- examples/query_parameters/python.py
+++ examples/query_parameters/python.py
@@ -1,11 +1,11 @@
-/***************************************************************
-Example of how to access query parameters in Python
-Important remark: query parameters accept multiple parameters
-under the same name. That's why there is an array defined for
-each existing parameter. You can always use index 0 to get the
-first parameter.
-Example call: https://<nufunction-url>?greeting=Hello&who=World
-***************************************************************/
+################################################################
+# Example of how to access query parameters in Python
+# Important remark: query parameters accept multiple parameters
+# under the same name. That's why there is an array defined for
+# each existing parameter. You can always use index 0 to get the
+# first parameter.
+# Example call: https://<nufunction-url>?greeting=Hello&who=World
+################################################################
 def handler(request):
     parameters = request['queryParameters']
     if not parameters is None and 'greeting' in parameters:
